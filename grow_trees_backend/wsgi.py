43bd21d744cda8adfc7a1f7b73e701f86fdd26b9
abhisaharan <abhi.saharan@gmail.com>
2019-11-19 23:03:42 -0800

    project structure set

--- grow_trees_backend/wsgi.py
+++ grow_trees_backend/wsgi.py
@@ -1,16 +1,14 @@
 """
-WSGI config for grow_trees_backend project.
+WSGI config for {{ project_name }} project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings.production")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grow_trees_backend.settings")
-
 application = get_wsgi_application()
